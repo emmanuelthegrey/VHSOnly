4920285d73434064e481bf502da7fa757af5efd7
emmanuel aubrey <whoisemmanuel@gmail.com>
2019-05-11 17:56:47 -0400

    Add project files.

---
+++ VHSOnly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VHSOnly.Startup))]
+namespace VHSOnly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
