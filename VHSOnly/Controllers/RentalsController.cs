55962763492cd53c2b547b2bf75c2eb0c58775f3
emmanuel aubrey <whoisemmanuel@gmail.com>
2019-05-30 00:24:49 -0400

    Intial rental form

---
+++ VHSOnly/Controllers/RentalsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHSOnly.Controllers
+{
+    public class RentalsController : Controller
+    {
+       
+        [Route("Rentals/NewRentalForm")]
+        public ActionResult New()
+        {
+            return View("NewRentalForm");
+        }
+    }
+}
